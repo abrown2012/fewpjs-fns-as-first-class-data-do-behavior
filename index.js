3e264abe8e3349a4fb936945bcaf785534885dd5
Agnieszka Brown <abrown2012@icloud.com>
2021-04-27 15:03:30 -0400

    commit tree e0b33a45ea048f30c13ed721e8e18162b7de7f56

--- index.js
+++ index.js
@@ -9,5 +9,26 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(provided_time) {
+  let greeting = ""
+  let my_hour = provided_time.split(':')[0]
+  if (my_hour < 12) {
+    greeting = "Good Morning"
+  }
+  else if (my_hour < 17) {
+    greeting = "Good Afternoon"
+  }
+  else {
+    greeting = "Good Evening"
+  }
+  return greeting
+}
+
+function displayMessage(message) {
+  let new_node 
+  new_node = document.getElementById("greeting");
+  new_node.innerText = message;
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
